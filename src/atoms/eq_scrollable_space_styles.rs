6af786e7f8558d83939a6bd6b97e62d519535285
Matthew Weisfeld <matthewedwardweisfeld@gmail.com>
2026-05-05 07:05:31 -0400

    Fix to scroll bar. Previous version only worked with the scrollbar plugin installed.

--- src/atoms/eq_scrollable_space_styles.rs
+++ src/atoms/eq_scrollable_space_styles.rs
@@ -4,8 +4,8 @@
 pub const CONTAINER: &str = "overflow-y-auto flex-1 min-h-0";
 
 /// Scrollbar styling - thin, themed.
-/// Tailwind v4 supports `scrollbar-thin` and custom scrollbar colors.
-pub const SCROLLBAR: &str = "scrollbar-thin scrollbar-thumb-[var(--color-scrollbar-thumb)] scrollbar-track-[var(--color-scrollbar-track)]";
+/// Tailwind v4 supports `scrollbar-thin` and custom scrollbar colors. However, this only works with the tailwindcss-scrollbar plugin so can't be used unless that is installed.
+pub const SCROLLBAR: &str = "[&::-webkit-scrollbar]:w-1 [&::-webkit-scrollbar-thumb]:bg-[var(--color-scrollbar-thumb)] [&::-webkit-scrollbar-track]:bg-[var(--color-scrollbar-track)] [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-track]:shadow-none";
 
 /// All style tokens for playground introspection.
 pub fn catalog() -> Vec<(&'static str, &'static str)> {
